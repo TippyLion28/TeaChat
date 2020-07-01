abf53fd1a7891ff1950e5800c29ff4dbde3d29d7
unknown <piguinea123@gmail.com>
2020-07-01 10:25:06 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,292 @@
+const express = require('express'); //WebApp module
+const app = express();
+const http = require('http').Server(app); //Express HTTP Server
+const io = require('socket.io')(http, { //Socket Library
+	pingInterval: 2500, //PingInterval at 2.5 seconds. Mobile clients may disconnect if this is above 5 seconds.
+	maxHttpBufferSize: 16*1000000 //Max message size before disconnect in bytes
+}); 
+const htmlspecialchars = require ('htmlspecialchars'); //For escaping HTML characters
+var isBase64 = require('is-base64'); //For checking if a string is valid base64
+const md = require('markdown-it')({ //For markdown parsing
+	html: false,
+	breaks: false,
+	linkify: true
+});
+//Configure markdown-it to open links in a new tab
+var defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {return self.renderToken(tokens, idx, options);};md.renderer.rules.link_open = function (tokens, idx, options, env, self) {var aIndex = tokens[idx].attrIndex('target');if (aIndex < 0) {tokens[idx].attrPush(['target', '_blank']);} else{tokens[idx].attrs[aIndex][1] = '_blank';}return defaultRender(tokens, idx, options, env, self);}; 
+
+//Configurables
+const maxNameLength = 32; //Maximum length of nicknames and room names etc.
+const maxMessageLength = 1000; //Maximum length of chat messages
+
+app.get('/', function(req, res) {
+    res.render('index.ejs'); //Render this page as response to HTTP get
+});
+app.use(express.static('public')); //Include the css folder
+io.sockets.on('connection', function(client) { //On connection
+
+	function sendMessage(dst, message, type) { //Function which sends a message to the specified clients with optional text fanciness
+		var isQuiet = false; //Specify whether the message should trigger a notification
+		switch(type) //Specify the type of message for added decoration
+		{
+			case 'subtle':
+				output = '<i style="color: grey;">' + message + '</i>';
+				isQuiet = true;
+			break;
+			case 'warning':
+				output = '⚠️ ' + message;
+			break;
+			case 'urgent':
+				output = '<b style="color: red">' + message + '</b>';
+			break;
+			default: //If the type of message is unspecified
+				output = message;
+			break;
+		}
+		switch(dst) //Specify where the message should go
+		{
+			case 'client':
+				client.emit('chat_message', output, isQuiet);
+			break;
+			case 'others':
+				client.to(client.roomID).emit('chat_message', output, isQuiet);
+			break;
+			case 'room':
+				io.in(client.roomID).emit('chat_message', output, isQuiet);
+			break;
+			case 'server':
+				io.emit('chat_message', output, isQuiet);
+			break;
+			default:
+				throw 'Invalid/Unspecified Destination'; //If no destination is specified, throw an exception
+			break;
+		}
+	}
+	
+	function chat(message){ //Turn chat messages into a function so it can be used by commands logic (for emotes etc)
+		sendMessage('room', '<strong>' + client.username + '</strong>: ' + message); //Send a message appearing to come from this client
+	}
+	
+	function announceJoin(){ //Announce the onboarding
+		sendMessage('others', client.username + ' has joined the room...', 'subtle');
+	}
+	function announceLeave(){ //Anounce client leaving
+		sendMessage('others', client.username + ' has left the room...', 'subtle');
+	}
+	
+	function clean(name){
+		if (name === null || name === undefined || name.length > maxNameLength) //If the supplied username is null or undefined, set it to an empty string in order to avoid crashes
+		{
+			name = '';
+		}
+		else
+		{
+			name = name.replace(/[^a-zA-Z0-9 ]/g, ''); //Remove symbols except spaces and numbers
+			name = htmlspecialchars(name); //Escape HTML
+			name = name.trim(); //Remove leading and trailing whitespace
+			name = name.replace(/ +(?= )/g,''); //Collapse multiple spaces down to one because trim is dopey
+		}
+		return name;
+	}
+
+    client.on('join', function(username,roomID) { //When credentials are recieved
+        client.username = clean(username);
+        if (client.username === '' || username === undefined ) //If the username is empty or just whitespace
+        {
+            //Kick the client
+			client.emit('authenticated',false);
+			console.log(client.request.connection.remoteAddress + ' was refused authentication (Invalid Username)');
+            client.disconnect();
+        }
+		else if (roomID === undefined)
+		{
+			client.emit('authenticated',false);
+			console.log(client.request.connection.remoteAddress + ' was refused authentication (Unspecified Room Number)');
+            client.disconnect();
+		}
+		else {
+            //Authenticate client and announce the onboarding
+			client.roomID = clean(roomID); //Sanitise the room ID
+			client.join(client.roomID); //Join the client to the room
+			client.emit('roomChange', client.roomID); //Tell the client to update their local copy of the room ID
+			client.emit('authenticated',true,client.username); //Tell the client they have authenticated
+            console.log(client.username + ' (' + client.request.connection.remoteAddress + ') has connected');
+			announceJoin();
+        }
+    });
+
+    client.on('disconnect', function(username) { //When the client disconnects
+        if (client.username === '' || client.username === undefined) //If the username was invalid, don't bother announcing to other clients
+        {
+            //Do nothing
+        } 
+		else {
+            //Announce the disconnect
+            console.log(client.username + ' (' + client.request.connection.remoteAddress + ') has disconnected');
+			announceLeave();
+        }
+    })
+	
+	client.on('image', function(base64){ //When the client sends an image
+	try{
+		if (isBase64(base64, {mimeRequired: true}) === true){ //If the string is a valid piece of base64
+			chat('<p><img onload="scrollDown()" class="chatImage" src="' + base64 + '" alt="[Invalid Image]" /></p>'); //Send the image and trigger the clientside scrollDown function
+		}
+		else{
+			sendMessage('client', 'Invalid image!', 'warning'); //If the base64 is invalid, don't send the image
+		}
+	}
+	catch{
+		sendMessage('client', 'Due to a <a href="https://github.com/nodejs/node/issues/759" target="_blank">bug,</a> images larger than ~3MB are not supported!', 'warning'); //Due to a bug in V8, large regex operations can cause crashes
+	}
+	});
+
+    client.on('chat_message', function(message) { //When a chat message is recieved
+        if (client.username !== undefined) //Check whether the client is valid
+        {
+			if (message.length > maxMessageLength)
+			{
+				sendMessage('client', 'Message is too long!', 'warning'); //Tell the client that their empty message won't be submitted
+			}
+			else if (message.trim() === '') //If message is empty after a trim
+			{
+				sendMessage('client', 'Empty messages won\'t be submitted!', 'warning'); //Tell the client that their empty message won't be submitted
+			}
+			else if (message.charAt(0) === '/') //If the message begins with a slash, it's a command
+			{
+				message = message.split(" ") //Break the command up into words
+				message[0] = message[0].toLowerCase(); //Convert the command to lowercase
+				switch (message[0]) {
+					//Commands List
+					case '/commands':
+						sendMessage('client', '<u>Commands:</u><br/>/commands - Show a list of commands<br/>/help - Show some help<br/>/join [Room Name] - Connect to the specified room. Leave room name blank to return to the main lobby<br/>/room - Show the current room name<br/>/emotes - Show a list of emoticons<br/>');
+					break;
+					
+					//General Commands
+					case '/help':
+						sendMessage('client', 'Welcome to the NodeJS powered old-school chatroom. Type /commands for a list of commands.');
+					break;
+					case '/join':
+						message[0] = ''; //Remove the /join command from the message, so the rest of the command can be used as the room name
+						message = message.join(" "); //Join the command array back together as a string
+						message = clean(message); //Sanitise the room name
+						if (message === client.roomID){ //If the client is already in the requested room
+							if (message === '') //If the room ID is unspecified
+							{
+								sendMessage('client', 'You are already in the main lobby', 'warning');
+							}
+							else
+							{
+								sendMessage('client', 'You are already in this room', 'warning');
+							}
+						}
+						else
+						{
+							announceLeave(); 
+							client.leave(client.roomID);
+							client.roomID = message;
+							client.join(client.roomID);
+							announceJoin();
+							client.emit('roomChange', client.roomID); //Tell the client to update their local copy of the room ID
+							if (client.roomID === '')
+							{
+								sendMessage('client','You have joined the main lobby', 'subtle');
+								console.log(client.username + ' has joined the main lobby');
+							}
+							else
+							{
+								sendMessage('client', 'You have joined the room: ' + client.roomID, 'subtle');
+								console.log(client.username + ' has joined room: ' + client.roomID)
+							}
+						}
+					break;
+					case '/room': //Remind the client which room they are in
+						if (client.roomID === '')
+						{
+							sendMessage('client', 'You are in the main lobby');
+						}
+						else
+						{
+							sendMessage('client', 'You are in the room: ' + client.roomID);
+						}
+					break;
+					
+					//Emotes
+					case '/emotes':
+						sendMessage('client', '/shrug - ¯\\_(ツ)_/¯<br/>/tableflip - (╯°□°）╯︵ ┻━┻<br/>/lenny  - ( ͡° ͜ʖ ͡°)<br>/lod - ಠ_ಠ');
+					break;
+					case '/lenny':
+						chat('( ͡° ͜ʖ ͡°)');
+					break;
+					case '/shrug':
+						chat('¯\\_(ツ)_/¯');
+					break;
+					case '/tableflip':
+						chat('(╯°□°）╯︵ ┻━┻');
+					break;
+					case '/lod':
+						chat('ಠ_ಠ');
+					break;
+						
+					//Fallback
+					default:
+						sendMessage('client', 'Unknown command - Type /commands for a list of valid commands.');
+					break;
+				}
+				
+			}
+			else
+			{
+				//Send message to all clients after stripping html tags and then linkifying the text
+				chat(md.renderInline(htmlspecialchars(message)));
+			}
+        }
+    });
+});
+
+//Create the server
+const server = http.listen(8080, function() {
+    console.log('Listening on *:8080');
+});
+
+//Announce the server shutdown when CTRL+C is pressed
+process.on('SIGINT', function() {
+	console.log('Server Closing');
+	io.emit('chat_message','<b style="color: red">⚠️ SERVER IS SHUTTING DOWN</b>');
+	process.exit();
+});
+
+//Command Line Interface
+var stdin = process.openStdin();
+stdin.addListener("data", function(command) {
+		var words = command.toString() //Convert the raw stdin stream into a string
+		words = words.replace(/\r?\n|\r/g, ""); //Get rid of the newline weirdness created by stdin
+		words = words.split(" "); //Break the command down into words
+		switch(words[0]) //Get the first word of the command
+		{
+			//Commands List
+			case 'help':
+				console.log('*Commands List*\nhelp - Show a list of commands\nsay [message] - Say a message to the server\nrooms - Show a list of rooms\nrefresh - Refresh all connected clients');
+			break;
+			
+			//General Commands
+			case 'say':
+				words[0] = '';
+				words = words.join(" ");
+				io.emit('chat_message','<b style="color: red">[SERVER]: ' + words + '</b>');
+			break;
+			case 'rooms':
+				console.log(Object.keys(io.sockets.adapter.rooms));
+			break;
+			case 'refresh':
+				console.log('Announcing the refresh');
+				io.emit('chat_message','<b style="color: red">⚠️ The server has issued a refresh. Your browser will refresh in a few seconds.</b>');
+				setTimeout(function(){io.emit('refresh'); console.log('Clients have been refreshed')}, 3000);
+			break;
+			
+			//Fallback
+			default:
+				console.log('Unknown command - Type help for a list of valid commands.');
+			break;
+		}
+});
---
+++ public/chatClient.js
@@ -0,0 +1,192 @@
+/* Variables */
+var username = prompt('Please enter a nickname'); //Ask for a nickname (This is done before making a connection to avoid timeouts)
+var focused = true;  //Whether document is focused (Set via event Listener)
+var newMessages = 0; //Number of new messages since the window was last focused
+const originalTitle = document.title; //Keep the original page title
+var allowReconnect = false; //Whether the client should attempt to reconnect on connection loss
+var roomID = ''; //Remember the room ID so we can re-join the correct room in case of a disconnection
+var autoScroll = true; //Whether to automatically scroll to the bottom of the page when a new message comes through
+var messageHistory = [];
+var messageFuture = [];
+
+/*Functions*/
+function unFocus() { //When the window goes out of focus
+	focused = false;
+}
+function focus(){ //When the window comes into focus
+	focused = true;
+	reJoin(); //This will reconnect us if we lost connection while the window was unfocused. Useful for mobile clients.
+	newMessages = 0;
+	document.title = originalTitle;
+}
+
+function scrollDown(){ //Create function for scrolling to the bottom of the page
+	if (autoScroll === true){
+		window.scrollTo(0,document.body.scrollHeight);
+	}
+}
+
+window.onscroll = function(e) { //Whenever the page is scrolled
+	if (this.oldScroll > this.scrollY && $(document).height() > $(window).height()){ //If the user scrolled up and there is a scrollbar on screen
+		autoScroll = false; //Disable auto scrolling
+	}
+	else if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight){ //If the user scrolls to the bottom of the chat
+		autoScroll = true; //Enable autoscrolling again
+	}
+	this.oldScroll = this.scrollY;
+};
+
+function join(){ // Create a function for submitting the nickname and room ID
+	server.emit('join', username, roomID);
+}
+
+function showLoader(bool){ //Create a function for hiding and showing the loader
+	if (bool === false)
+	{
+		$("#loader").hide();
+	}
+	else
+	{
+		$("#loader").show();
+	}
+}
+
+function sendFile() {
+	const file = document.getElementById('selectedFile').files[0]; //Grab the file
+	const reader = new FileReader(); //Create a new file reader
+	
+	reader.addEventListener("load", function () { //When file is read by the file reader
+		server.emit('image',reader.result); //Send base64 image to the server
+		autoScroll = true; //Make sure the page can scroll down once the photo picker window has closed
+	}, false);
+
+	if (file) { //If file is set
+		reader.readAsDataURL(file); //Send file to the file reader
+	}
+}
+
+/*Socket Code*/
+var server = io.connect(window.location.host, { //Specify the connection paramaters (window.location.host gets the hostname of the server)
+	reconnection:false //Do not reconnect automatically, we will handle this manually
+});
+
+join(); //Once the variables and paramaters are initialised, join the server
+
+server.on('authenticated', function(authed,authUsername) //When server has sent an authentication reply and sanitised username
+{
+	if (authed === true) //If the server has told us we are authenticated
+	{
+		if (allowReconnect === false){ //If this is the first time authenticating
+			username = authUsername; //Set the local username to the server sanitised username
+			$('#messages').append($('<li>').html('🟢 Welcome to the chat, ' + username + '! Type /help for information!'));
+			allowReconnect = true; //The server has allowed us to connect once, therefore we should be able to re-connect using these credentials
+		}
+		else if (allowReconnect === true){
+			$('#messages li:last-child').remove(); //Remove the disconnect message
+			$('#messages').append($('<li style="color: grey; font-style:italic;">').html('You have been reconnected to the chat')); //Tell the user we reconnected
+		}
+		$("#submitTxt").attr("disabled", false); //Enable buttons
+		$("#submitImage").attr("disabled", false);
+	}
+	else if (authed === false) //If we didn't get auth (The server will terminate the connection at this point)
+	{
+		allowReconnect = false; //The server didn't authenticate us using these credentials, no point trying again
+	}
+	scrollDown();
+	showLoader(false);
+});
+
+server.on('roomChange', function(room) //When the server tells us we switched rooms
+{
+	roomID = room; //Save the current room ID in case we need it for reconnecting
+});
+
+$('form').submit(function(e){ //Submit a chat message to the server
+	e.preventDefault(); //Prevents page reloading
+	server.emit('chat_message', $('#txt').val()); //Send the message to the server
+	if ($('#txt').val() !== '' && $('#txt').val() != messageHistory[messageHistory.length -1]){ //If the message isn't empty and isn't the same as the previous message
+		messageHistory.push($('#txt').val()); //Store the message to the history
+	}
+	$('#txt').val(''); //Empty the text box
+	autoScroll = true; //Re-enable auto scrolling
+	return false;
+});
+
+server.on('chat_message', function(msg, isQuiet){ // Append recieved messages to the list
+	$('#messages').append($('<li>').html(msg));
+	scrollDown();
+	if (focused === false && (isQuiet == false || isQuiet === undefined)){ //If the page isn't focused and the message isn't quiet, add a message counter to the HTML title
+		newMessages++;
+		document.title = '(' + newMessages + ') ' + originalTitle;
+	}
+});
+
+server.on('disconnect', function(){ //Detect connection loss
+	$("#submitTxt").attr("disabled", true); //Disable buttons
+	$("#submitImage").attr("disabled", true);
+	if (allowReconnect === true)
+	{
+		$('#messages').append($('<li onclick="reJoin()" style="cursor: pointer;">').html('⚠️ Connection Lost - Click this message to try again'));
+		document.title = '(Disconnected) ' + originalTitle;
+	}
+	else
+	{
+		$('#messages').append($('<li onclick="location.reload();" style="cursor: pointer;">').html('⚠️ Server refused connection - Click this message to retry'));
+	}
+	scrollDown();
+});
+
+server.on('refresh', function(){ //When the server tells us to refresh
+	window.setTimeout(function(){location.reload()}, 1000);
+});
+
+function reJoin(){ //Create function to rejoin and check connectivity
+var count = 0; //Start retry counter at zero
+if (server.connected === false && allowReconnect === true && focused === true){
+	showLoader(true); //Show a loading spinner
+	server.open(); //Attempt to open the connection
+	function checkConnectivity() {
+		if (count >= 5) //Check connection this many times
+		{
+			//Stop checking and hide the loading spinner
+			showLoader(false);
+		}
+		else if(server.connected === false) {
+			count = count+1;
+			window.setTimeout(checkConnectivity, 1000); //Check the flag every second
+		} 
+		else if (server.connected === true) { //If the we managed to connect
+			join();
+		}
+	}
+	checkConnectivity(); //Check for connectivity
+	}
+}
+
+/*Events*/
+window.onresize = function(event) { //When the keyboard is opened on mobile, scroll down
+	scrollDown();
+};
+
+document.onkeydown = function(event) {
+	//Message history works by storing all past messages in an array. When cycling through previous messages using the arrow keys, the messages are shuffled between two arrays, messageHistory and messageFuture. When the user begins modifying or submits one of their previous messages, the two arrays are then concatenated back into messageHistory.
+    if (event.keyCode == '38') { // up arrow
+		$('#txt').val(messageHistory[messageHistory.length -1]); //Put the previous message into the text box
+		if (messageHistory.length != 0){ //If the history is not empty
+			messageFuture.push(messageHistory.pop()); //Shuffle the messages as the user cycles through old messages
+		}
+    }
+    else if (event.keyCode == '40') { //down arrow
+		if (messageFuture.length != 0){ //If the message future is not empty
+			messageHistory.push(messageFuture.pop()); //Shuffle the messages as the user cycles through the next messages
+		}
+		$('#txt').val(messageFuture[messageFuture.length -1]); //Put the next message into the text box
+    }
+	else { //If the user presses any other key, assume the message has been sent or modified
+		messageHistory = messageHistory.concat(messageFuture.reverse()); //Join all of the messages back into the first array
+		messageFuture = []; //Empty the second array
+	}
+}
+
+window.addEventListener('blur', unFocus); //When window is unfocused
+window.addEventListener('focus', focus); //When window is focused
